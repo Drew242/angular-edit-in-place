db9e24c1a48d5b2a9e0208f73c9a62af9ba75329
Drew Conly <atconly@gmail.com>
2016-07-13 14:57:59 -0600

    make it look like hello sticker

--- user.js
+++ user.js
@@ -7,18 +7,18 @@ angular.module('UserApp')
   ]);
 
 function userCtrl($http) {
-  var user       = this;
-  user.inputFn   = false;
-  user.inputLn   = false;
-  user.inputE    = false;
-  user.showFn    = true;
-  user.showLn    = true;
-  user.showE     = true;
-  user.message   = "Edit User";
-  user.intro     = "Profile Name";
-  user.firstname = "John";
-  user.lastname  = "Doe";
-  user.email     = "john.doe@aol.com";
+  var user        = this;
+  user.inputFn    = false;
+  user.inputLn    = false;
+  user.inputE     = false;
+  user.showFn     = true;
+  user.showLn     = true;
+  user.showE      = true;
+  user.message    = "Hello";
+  user.subMessage = "my name is";
+  user.firstname  = "John";
+  user.lastname   = "Doe";
+  user.email      = "john.doe@aol.com";
 
   user.editFn = function() {
       user.inputFn = !user.inputFn;
