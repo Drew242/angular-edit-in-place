f8705aa8962c497acdd40791629f31f012363df1
Drew Conly <atconly@gmail.com>
2016-07-13 14:14:11 -0600

    edit fields appear

--- user.js
+++ user.js
@@ -8,9 +8,26 @@ angular.module('UserApp')
 
 function userCtrl($http) {
   var user       = this;
+  user.inputFn   = false;
+  user.inputLn   = false;
+  // user.inputE    = false;
   user.message   = "Edit User";
   user.intro     = "Profile Name";
-  user.firstname = "";
-  user.lastname  = "";
-  user.email  = ""; 
+  user.firstname = "John";
+  user.lastname  = "Doe";
+  user.email     = "john.doe@aol.com";
+
+  user.editFn = function() {
+    console.log('editFn');
+      user.inputFn = !user.inputFn;
+  }
+  user.editLn = function() {
+    console.log('editLn');
+      user.inputLn = !user.inputLn;
+  }
+  user.editE = function() {
+    console.log('editE');
+      user.inputE = !user.inputE;
+  }
+
 }
